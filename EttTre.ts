ce8f17e8f1e3388e7d5230e6ea5a87f8ddc13cfe
hackerbuddy229 <rasmusb229@gmail.com>
2020-09-11 13:29:00 +0200

    1.5

--- EttTre.ts
+++ EttTre.ts
@@ -1,10 +1,10 @@
 const colorList : string[] = ["red", "green", "yellow", "blue"];
 
 function start():void {
-    const interations:number = 10000;
+    const interations:number = 500;
 
     for (let index:number = 0; index < interations; index++){
-        rectangle(random(200), random(200), 10, 10, getRandomColor());
+        line(random(200), random(200), random(200), random(200), 2, getRandomColor());
     }
 }
 
@@ -18,4 +18,5 @@ function getRandomColor():string{
 
 function rectangle(x,y,z,a,b):void{}
 function random(x:number):number{return 0;}
+function line(x1,y1,x2,y2,r,c):void{}
 function randomElement(x:string[]):string{return "";}
